3a9311a64d87ba1e8ba4f80bdf787b7772537099
fast050 <dragonman050@gmail.com>
2022-02-17 00:16:33 +0400

    -fix some bugs

--- app/src/main/java/com/example/basicbankingapp/BankApplication.kt
+++ app/src/main/java/com/example/basicbankingapp/BankApplication.kt
@@ -1,6 +1,7 @@
 package com.example.basicbankingapp
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.basicbankingapp.data.UserDatabase
 
 class BankApplication:Application()
@@ -8,5 +9,12 @@ class BankApplication:Application()
 
     private val database by lazy { UserDatabase.getInstance(applicationContext)}
     val dao by lazy { database.userDao() }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+    }
 
 }
